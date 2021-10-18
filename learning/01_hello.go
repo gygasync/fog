b7a49a13019e9ccd9bac5b89b6e3e05f446e9be5
Igor Pejakovic <gygasync@gmail.com>
2021-10-18 22:38:37 +0200

    Add Hello world example

---
+++ learning/01_hello.go
@@ -0,0 +1,9 @@
+package learning
+
+import "fmt"
+
+var who = "World"
+
+func Hello() {
+	fmt.Println("Hello " + who)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fog/learning"
+
+func main() {
+	learning.Hello()
+}
