9962f968918526de58ff58487adedbf28cb30fb2
Igor Pejakovic <gygasync@gmail.com>
2021-10-23 18:05:09 +0200

    Update schema

--- db/migrations/1_install.up.sql
+++ db/migrations/1_install.up.sql
@@ -1,6 +1,5 @@
-
 CREATE TABLE Directory ( 
-	Id                   varchar(36) NOT NULL  PRIMARY KEY  ,
+	Id                   varbinary(32) NOT NULL  PRIMARY KEY  ,
 	Path                 text NOT NULL    ,
 	Dateadded            datetime  DEFAULT CURRENT_TIMESTAMP   ,
 	Lastchecked          datetime     
@@ -9,13 +8,12 @@ CREATE TABLE Directory (
 CREATE INDEX Idx_Directory ON Directory ( Id );
 
 CREATE TABLE File ( 
-	Id                   varchar(36) NOT NULL  PRIMARY KEY  ,
+	Id                   varbinary(32) NOT NULL  PRIMARY KEY  ,
 	Path                 text     ,
-	Parentdirectory      varchar(36) NOT NULL    ,
+	Parentdirectory      varbinary(32) NOT NULL    ,
 	Checksum             binary(32)     ,
 	Lastchecked          datetime     ,
 	FOREIGN KEY ( Parentdirectory ) REFERENCES Directory( Id )  
  );
 
 CREATE INDEX Idx_File ON File ( Id );
-
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require rsc.io/quote v1.5.2
 
 require (
 	github.com/golang-migrate/migrate/v4 v4.15.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.9 // indirect
