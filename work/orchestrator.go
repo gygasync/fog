2ebbfad48ea13f6555b77e1fe558f0256506e076
Igor Pejakovic <gygasync@gmail.com>
2022-01-20 20:39:22 +0100

    Make orchestrator singleton.

--- work/orchestrator.go
+++ work/orchestrator.go
@@ -1,8 +1,38 @@
 package work
 
-import "fog/work/definition"
+import (
+	"fog/common"
+	"fog/work/definition"
+	"sync"
+
+	"github.com/streadway/amqp"
+)
+
+var once sync.Once
+var instance orchestrator
 
 type IOrchestrator interface {
 	PublishWork(work *definition.Work)
 	StartResponseQeue(comms definition.Communication, response responseQueue)
+}
+
+type orchestrator struct {
+	connection *amqp.Connection
+	logger     common.Logger
+}
+
+func NewOrchestrator(connection string, logger common.Logger) orchestrator {
+	once.Do(func() {
+		conn, err := amqp.Dial(connection)
+		if err != nil {
+			logger.Error("Failed to create orchestrator", err)
+			panic(err)
+		}
+		instance = orchestrator{
+			connection: conn,
+			logger:     logger,
+		}
+	})
+
+	return instance
 }
